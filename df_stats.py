8b438add4a6bd3b4589bb634de9535335570a6e8
Jean Remacle <git@remacle.eu>
2021-06-05 14:05:10 +0200

    first commit

---
+++ df_stats.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+
+with open('speedtest.json') as f:
+    content = [json.loads(line.rstrip('\n')) for line in f]
+
+df_dict = []
+for item in content:
+    try:
+        t_dict = {"timestamp": item["timestamp"],
+            "jitter":    item["ping"]["jitter"],
+            "latency":   item["ping"]["latency"],
+            "download":  item["download"]["bandwidth"] / 1024 / 1024 * 8,
+            "upload":    item["upload"]["bandwidth"] / 1024 / 1024 * 8,
+            "packetLoss": item["packetLoss"]}
+    except KeyError as e:
+        print(item)
+        print(e)
+        continue
+    df_dict.append(t_dict)
+
+df = pd.DataFrame.from_dict(df_dict)
+
+ax = df.plot(subplots=True, figsize=(8, 8))
+plt.legend(loc='best')
+plt.savefig('test_graph_2.jpg', bbox_inches='tight')
---
+++ mon_speed.cmd
@@ -0,0 +1,1 @@
+speedtest --server-id=18136 --format=json --unit=Mibps --progress=no  >> C:\localapp\logs\speedtest.json
